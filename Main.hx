337451ed68475108923cdb38c444b8d59ad320f3
Crinfarr <contact@hecancanyou.xyz>
2021-09-25 13:14:11 -0500

    keep working on whatever this is

--- Main.hx
+++ Main.hx
@@ -1,3 +1,4 @@
+import haxe.io.BytesData;
 import sys.io.File;
 import haxe.zip.Uncompress;
 import haxe.io.Bytes;
@@ -6,13 +7,28 @@ import lzma.Lzma.LZMA;
 import Sys;
 
 class Main {
+	private static function binToBytes(A:Array<Bool>):Bytes {
+		var out:BytesData = [];
+		var b:Int=0;
+		while (A.length > 0) {
+			for (i in 0...8) {
+				b += (A.shift()?1<<i:0);
+			}
+			out.push(b);
+			trace(b);
+			b=0;
+		}
+		return Bytes.ofData(out);
+	}
 	public static function main() {
-		var shipbytes = Base64.decode(Sys.args()[0].substr(19));
+		var shipbytes = Base64.decode(Sys.args()[0]);
+		//trace(Base64.decode(Sys.args()[0]).toHex());
 		var shipdata = LZMA.lzma(shipbytes);
 		var bin = "";
 		for (i in 0...shipdata.length) {
 			bin += (shipdata[i]?"1": "0");
 		}
 		trace(bin);
+		trace(binToBytes(shipdata).toHex());
 	}
 }
