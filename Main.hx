f218fcd2a2222c16686c96d0b8573ee96d9f6c5c
Crinfarr <contact@hecancanyou.xyz>
2021-09-25 12:36:15 -0500

    add functionality

--- Main.hx
+++ Main.hx
@@ -7,70 +7,12 @@ import Sys;
 
 class Main {
 	public static function main() {
-		var shipbytes = Bytes.ofString(Sys.args()[0]);
-		var ship64 = Base64.decode(Sys.args()[0]);
-		var shipdata = [];
-
-		trace('string stats: \n  Length: ${Sys.args()[0].length}\n\n');
-
-		trace("Trying raw bytes...");
-
-		for (byte in 0...shipbytes.length) {
-			shipdata[byte] = StringTools.hex(shipbytes.get(byte));
+		var shipbytes = Base64.decode(Sys.args()[0].substr(19));
+		var shipdata = LZMA.lzma(shipbytes);
+		var bin = "";
+		for (i in 0...shipdata.length) {
+			bin += (shipdata[i]?"1": "0");
 		}
-		trace("ship data: " + shipdata);
-		trace("data length: " + shipdata.length);
-
-		try {
-			shipbytes = Uncompress.run(shipbytes);
-			trace("BZIP success, output: ");
-			shipdata = [];
-			for (byte in 0...shipbytes.length) {
-				shipdata[byte] = StringTools.hex(shipbytes.get(byte));
-			}
-			trace(shipdata + "\n\n");
-		} catch (err) {
-			trace("BZIP failed\n");
-			trace(err);
-		}
-
-		trace("Trying base64 decoded bytes...");
-
-		shipdata = [];
-		for (byte in 0...ship64.length) {
-			shipdata[byte] = StringTools.hex(ship64.get(byte));
-		}
-		trace("ship data: " + shipdata);
-		trace("data length: " + shipdata.length);
-
-		try {
-			ship64 = Uncompress.run(ship64);
-			trace("BZIP success, output:");
-			shipdata = [];
-			for (byte in 0...ship64.length) {
-				shipdata[byte] = StringTools.hex(ship64.get(byte));
-			}
-			trace(shipdata + "\n\n");
-		} catch (err) {
-			trace("BZIP failed\n");
-			trace(err + "\n\n");
-		}
-		try {
-			var out = LZMA.lzma(ship64);
-			trace("LZMA complete, output: "+out);
-			var tuo=0;
-			for (z in 0...out.length) {
-				tuo += (out[z]==1?Math.floor(Math.pow(2, out.length-z)):0);
-			}
-			trace("as dec: "+tuo);
-			trace("as hex: "+StringTools.hex(tuo));
-
-		}
-		catch (err) {
-			trace("LZMA failed\n");
-			trace(err+"\n\n");
-		}
-
-		trace("All tests finished");
+		trace(bin);
 	}
 }
--- lzma/Lzma.hx
+++ lzma/Lzma.hx
@@ -1,11 +1,58 @@
 package lzma;
 
+import cpp.UInt32;
+import cpp.UInt16;
+import haxe.io.BytesData;
 import haxe.crypto.BaseCode;
 import haxe.io.BytesBuffer;
 import haxe.io.Bytes;
 
 class LZMA {
-    public static function lzma(input:Bytes) {
-        
+    public static function lzma(input:Bytes):Array<Bool> {
+        var bytes:BytesData = input.getData();
+        bytes.shift();
+
+        var prob:UInt = 0x400; // 0000 0000 0100 0000 0000, reset to 11 by prob&(0000 0000 0111 1111 1111) [2047] 
+
+        var code:UInt = ((bytes.shift()<<24)+(bytes.shift()<<16)+(bytes.shift()<<8)+(bytes.shift()));
+        //0000 0000 0000 0000 0000 0000 0000 0000
+        //00 00 00 00
+        //mask for last 8: 1111 1111 1111 1111 1111 1111 0000 0000
+        //            hex: 0xffffff00
+        var range:UInt = 0xffffffff;
+        var bound:UInt;
+        trace(code);
+        trace(range);
+
+        var output:Array<Bool> = [];
+        while (bytes.length > 0) {
+            if (range < (0x800000:UInt)) { //normalization
+                range = range<<8;
+                code = code<<8;
+
+                code = (code&(0xffffff00:UInt)+bytes.shift());
+            /**
+             * 0xffffff00 = 
+             *    1111 1111 1111 1111 1111 1111 0000 0000
+             * bytes.shift() =>                 xxxx xxxx
+             * sets least significant 8 figs to new byte 
+             */
+            }
+
+            bound = Math.floor(range/0x800)*prob;
+
+            if (code < bound) {
+                range = bound;
+                prob += Math.floor((0x800-prob)/0x10);
+                output.push(false);
+            }
+            else {
+                range -= bound;
+                code -= bound;
+                prob -= Math.floor(prob/0x10);
+                output.push(true);
+            }
+        }
+        return output;
     }
 }
